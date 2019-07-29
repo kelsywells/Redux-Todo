cd77ba402324ff49675151d59f868cb89d9ad541
Kelsy <kelsywells@gmail.com>
2019-07-28 18:38:16 -0700

    todo input responding, just need to get it displayed. Having a couple bugs. Brain is broken right now, will come back after a break

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,24 +1,50 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 class App extends React.Component {
 constructor(props) {
   super(props);
     this.state= {
-      todos: []
+      newTodo: ''
     }
   }
+
+  addTodo(e) {
+    e.preventDefault();
+    this.props.addTodo(
+      {
+        value: this.state.newTodo,
+        completed: false
+      }
+    )
+
+    this.setState(
+      {
+      newTodo: ''      
+    }
+    )
+
+  }
+
+  
+  addNewTodo = e => {
+    this.setState({
+     newTodo: e.target.value
+    })
+    console.log('newTodo value:', e.target.value)
+  }
+
+
 
 
     render() {
       return(
         <div>
-          <form>
+          <form onSubmit= {this.addTodo}>
             <input
-            type="text" placeholder="Add Todo">
 
-            </input>
+            type="text" placeholder="Add Todo" onChange= {this.addNewTodo} value={this.newTodo} />
+
           </form>
           <button>Submit Todo</button>
         </div>
